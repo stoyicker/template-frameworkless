ab4a1ebcff8328237b1727ce03b2c946f8b26e9e
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-09-09 12:45:12 +0200

    Complete OOfnLRUCache

--- src/main/kotlin/lrucache/LruCache.kt
+++
@@ -1,6 +0,0 @@
-package lrucache
-
-internal class LruCache {
-
-
-}
---
+++ src/main/kotlin/lrucache/OOfnLRUCache.kt
@@ -0,0 +1,38 @@
+package lrucache
+
+internal class OOfnLRUCache<T : Any>(private val maxSizeItems: Int) {
+    private var delegate = emptyMap<String, ValueWrapper<T>>()
+
+    fun put(key: String, value: T) {
+        synchronized(delegate) {
+            if (delegate.size >= maxSizeItems) {
+                removeLeastRecentlyUsed()
+                put(key, value)
+            } else {
+                delegate += key to ValueWrapper(value = value)
+            }
+        }
+    }
+
+    fun get(key: String): T? = delegate[key]?.get()
+
+    private fun removeLeastRecentlyUsed() {
+        delegate.entries.fold("", { currentKey, entry ->
+            val currentLRUValueWrapper = delegate[currentKey]
+            if (currentLRUValueWrapper?.usage ?: Long.MAX_VALUE >= entry.value.usage) {
+                currentKey
+            } else {
+                entry.key
+            }
+        }).let { delegate = delegate.minus(it) }
+    }
+
+    private class ValueWrapper<out T>(
+            var usage: Long = System.currentTimeMillis(),
+            private val value: T) {
+        fun get(): T {
+            usage = System.currentTimeMillis()
+            return value
+        }
+    }
+}
