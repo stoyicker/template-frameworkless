03bb4bf64ac134161958d50510ace4ddc81e62a4
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-09-10 18:15:36 +0200

    Add quicksort test

--- src/main/kotlin/main/Main.kt
+++ src/main/kotlin/main/Main.kt
@@ -1,6 +1,18 @@
 package main
 
+import quicksort.QuickSort
+
 fun main(args: Array<String>) {
     System.out.println("Hello project")
-
+    testQuickSort()
 }
+
+private fun testQuickSort() {
+    System.out.println("TEST QUICKSORT")
+    val array = arrayOf(3,9,6,-21,8,0,8912312,5)
+    System.out.println("Before sorting: ${arrayToString(array)}")
+    QuickSort.on(array)
+    System.out.println("After sorting: ${arrayToString(array)}")
+}
+
+private fun arrayToString(array: Array<Int>) = StringBuilder().apply { array.forEach { append("$it, ")} }
