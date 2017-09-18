c2665d809b6e396fa108c9ddd7adf68b0914aaee
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-09-18 12:42:03 +0200

    Permutations

--- src/main/kotlin/main/Main.kt
+++ src/main/kotlin/main/Main.kt
@@ -1,5 +1,10 @@
 package main
 
+import permutations.Permutations
+
 fun main(args: Array<String>) {
     System.out.println("Hello project")
+    Permutations.of("abc").forEach {
+        System.out.println(it)
+    }
 }
