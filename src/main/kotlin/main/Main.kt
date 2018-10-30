0276509057fa3fd3431e50fd6acc8a3e8e10298e
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2018-10-30 16:23:13 +0100

    Hashcode fun

--- src/main/kotlin/main/Main.kt
+++ src/main/kotlin/main/Main.kt
@@ -1,5 +1,13 @@
 package main
 
 fun main(args: Array<String>) {
-    System.out.println("Hello project")
+    val map = mutableMapOf<Item, String>()
+    while (true) {
+        map[Item("id")] = "value"
+    }
 }
+
+// If you remove "data" from here, you're also removing the hashCode implementation, so Object#hashCode
+// will be called, effectively giving you an ever-growing map above until the heap limit is reached
+// To observe, run with vm arg -Xmx1M (sets heap limit to 1MB)
+data class Item(private val field: String)
