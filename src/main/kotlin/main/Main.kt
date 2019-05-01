1e83c910ce341ed0b4fe048f68c7a82d925323a7
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2019-05-01 11:26:30 +0200

    Clean up bit merge

--- src/main/kotlin/main/Main.kt
+++ src/main/kotlin/main/Main.kt
@@ -3,8 +3,12 @@ package main
 import bit.Bit
 
 fun main(args: Array<String>) {
-    System.out.println(Bit.get(9, -1)) //false
-    System.out.println(Bit.set(8, 4)) // 24
-    System.out.println(Bit.clear(0, 0)) // 0
-    System.out.println(Bit.clear(9, 0)) // 8
+    bit()
+}
+
+private fun bit() {
+    System.out.println(Bit.get(25, -1)) //false: 25 is 1101, negative shifting becomes positive so you get index 1 which is 0
+    System.out.println(Bit.set(8, 4)) // 24: 8 is 100, 1100 is 24
+    System.out.println(Bit.clear(0, 0)) // 0: 0 is 0, becomes 0
+    System.out.println(Bit.clear(9, 0)) // 8: 9 is 101, becomes 100, which is 8
 }
