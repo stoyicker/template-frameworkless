58472db21ab7cb6bd2c0898525bac56ec4c7e8a9
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2019-06-08 09:50:26 +0200

    Update some comments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -25,7 +25,7 @@ fun main(args: Array<String>) {
 }
 
 private fun bit() {
-	System.out.println(Bits.getBit(25, -1)) //false: 25 is 1101, negative shifting becomes positive so you getBit index 1 which is 0
+	System.out.println(Bits.getBit(25, -1)) //0; 25 is 1101, negative shifting becomes positive so you getBit index 1 which is 0
 	System.out.println(Bits.setBit(8, 4)) // 24: 8 is 100, 1100 is 24
 	System.out.println(Bits.clearBit(0, 0)) // 0: 0 is 0, becomes 0
 	System.out.println(Bits.clearBit(9, 0)) // 8: 9 is 101, becomes 100, which is 8
@@ -33,9 +33,9 @@ private fun bit() {
 
 private fun hashcode() {
 	/**
-	 * Right now, this will never surpass size 1. However, if we remove "data" from here, we're also removing the
-	 * hashCode implementation, so Object#hashCode will be called, effectively giving you an ever-growing map every
-	 * iteration.
+	 * Right now, this will never surpass size 1. However, if we remove "data" from the Item class, we're also removing
+	 * the custom hashCode implementation, so Object#hashCode will be called, effectively giving you an ever-growing map
+	 * every iteration until it blows up the stack.
 	 */
 	val map = mutableMapOf<Item, String>()
 	repeat(1000000) {
